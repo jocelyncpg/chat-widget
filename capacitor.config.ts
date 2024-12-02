baab3163ed2bce4b028cc5a4ae682124de44786a
jocelyncpg <jocperezg@gmail.com>
2024-12-02 00:45:32 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'chat-widget',
+  webDir: 'www'
+};
+
+export default config;
